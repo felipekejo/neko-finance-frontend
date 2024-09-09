c96654a5da102528cb224a78fee413fc96e4b6de
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2024-09-09 23:33:09 +0930

    add sidebar

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Sidebar } from "@/components/Sidebar";
+
+export default function Home() {
+  return (
+  
+  <main className="flex h-screen w-full">
+    <Sidebar />
+    <h1>Home</h1>
+    <p>Welcome to your new Neko app!</p>
+  </main>
+  );
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import Image from "next/image"
+import Link from "next/link"
+
+export function Sidebar() {
+  return (
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col  justify-between border-r pt-8 max-md:hidden sm:p-4 xl:p-6 2xl:w-[355px]">
+    <nav className="flex flex-col gap-4">
+      <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+        <Image
+          src="./icons/neko.svg"
+          width={34}
+          height={34}
+          alt="Horizontal Logo"
+          className="size=[24px] max-xl:text-[14px] max-xl:leading-[20px] text-logo-1"
+        />
+        <h1 className="2xl:text-[26px] 2xl:leading-[32px] font-ibm-plex-serif font-bold text-black-1 max-xl:hidden">
+          NekoFinance
+        </h1>
+      </Link>
+
+    </nav>
+    FOOTER
+  </section>
+  )
+}
