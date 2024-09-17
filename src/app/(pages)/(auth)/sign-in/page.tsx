f5e44f87b90dc220f00dfac5fbd90be93a9ef8d7
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2024-09-17 23:19:00 +0930

    fixed react hook forms

--- src/app/(pages)/(auth)/sign-in/page.tsx
+++ src/app/(pages)/(auth)/sign-in/page.tsx
@@ -1,35 +1,53 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Suspense } from "react";
-import { useForm } from "react-hook-form";
+'use client'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Suspense } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const singInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof singInForm>
 
 export default function SignIn() {
-  const {register, handleSubmit} = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>({})
 
-  function handleSignIn(){}
+  async function handleSignIn(data: SignInForm) {
+    console.log(data)
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+  }
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <div className="p-8 bg-background">
         <div className="w-[350px] flex flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Access NekoFinance</h1>
-            <p className="text-sm text-muted-foreground">Control your expenses</p>
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Access NekoFinance
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Control your expenses
+            </p>
           </div>
           <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label>Your E-mail</Label>
-              <Input type="email" id="email " {...register("email")}/>
+              <Input type="email" id="email " {...register('email')} />
             </div>
-            <Button className="w-full" type="submit">Sign In</Button>
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Sign In
+            </Button>
           </form>
-
-          
         </div>
       </div>
     </Suspense>
-    
   )
 }
